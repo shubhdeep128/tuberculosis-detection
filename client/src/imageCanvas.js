50e5128360f9a12e57e88d7e39a23fa2e3c5cb1d
Joe2k <jonathansamuel2k@gmail.com>
2021-03-23 14:27:58 +0530

    new cropping tool

--- client/src/imageCanvas.js
+++ client/src/imageCanvas.js
@@ -2,6 +2,9 @@ import "./App.css";
 import { useState, useEffect } from "react";
 import base64 from "base-64";
 import CanvasDraw from "react-canvas-draw";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
 
 function App(props) {
   let saveableCanvas;
@@ -13,17 +16,49 @@ function App(props) {
   }, []);
   return (
     url && (
-      <div className="App">
+      <Container>
+        <Row></Row>
+        <h1 style={{ fontSize: "26px", marginTop: "20px" }}>
+          Draw around the boils/scars and click on submit
+        </h1>
         <CanvasDraw
           ref={(canvasDraw) => (saveableCanvas = canvasDraw)}
           imgSrc={url}
+          brushRadius={2}
+          hideGrid={true}
+          brushColor="#fff"
+          style={{ margin: "20px" }}
+          canvasHeight={400}
+          canvasWidth={300}
         ></CanvasDraw>
-        <button
+        <Button
+          style={{ margin: "5px" }}
+          variant="warning"
+          onClick={() => {
+            console.log(saveableCanvas.undo());
+          }}
+        >
+          Undo
+        </Button>
+        <Button
+          style={{ margin: "5px" }}
+          variant="danger"
+          onClick={() => {
+            console.log(saveableCanvas.clear());
+          }}
+        >
+          Clear
+        </Button>
+        <Button
+          style={{ display: "block" }}
           onClick={() => {
             console.log(saveableCanvas.getSaveData());
+            window.ReactNativeWebView.postMessage("Hello!");
           }}
-        ></button>
-      </div>
+        >
+          Submit
+        </Button>
+      </Container>
     )
   );
 }
