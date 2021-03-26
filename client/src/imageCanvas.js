b983cc6ca414c48b250c6667297698c373e70068
Joe2k <jonathansamuel2k@gmail.com>
2021-03-26 22:18:10 +0530

    add webview

--- client/src/imageCanvas.js
+++ client/src/imageCanvas.js
@@ -5,6 +5,7 @@ import CanvasDraw from "react-canvas-draw";
 import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 function App(props) {
   let saveableCanvas;
@@ -16,48 +17,66 @@ function App(props) {
   }, []);
   return (
     url && (
-      <Container>
-        <Row></Row>
-        <h1 style={{ fontSize: "26px", marginTop: "20px" }}>
-          Draw around the boils/scars and click on submit
-        </h1>
-        <CanvasDraw
-          ref={(canvasDraw) => (saveableCanvas = canvasDraw)}
-          imgSrc={url}
-          brushRadius={2}
-          hideGrid={true}
-          brushColor="#fff"
-          style={{ margin: "20px" }}
-          canvasHeight={400}
-          canvasWidth={300}
-        ></CanvasDraw>
-        <Button
-          style={{ margin: "5px" }}
-          variant="warning"
-          onClick={() => {
-            console.log(saveableCanvas.undo());
-          }}
-        >
-          Undo
-        </Button>
-        <Button
-          style={{ margin: "5px" }}
-          variant="danger"
-          onClick={() => {
-            console.log(saveableCanvas.clear());
-          }}
-        >
-          Clear
-        </Button>
-        <Button
-          style={{ display: "block" }}
-          onClick={() => {
-            console.log(saveableCanvas.getSaveData());
-            window.ReactNativeWebView.postMessage("Hello!");
-          }}
-        >
-          Submit
-        </Button>
+      <Container fluid>
+        <Row>
+          <Col className="justify-content-md-center">
+            <h1 style={{ fontSize: "26px", marginTop: "20px" }}>
+              Draw around the boils/scars and click on submit
+            </h1>
+          </Col>
+        </Row>
+        <Row>
+          <Col className="justify-content-md-center">
+            <CanvasDraw
+              ref={(canvasDraw) => (saveableCanvas = canvasDraw)}
+              imgSrc={url}
+              brushRadius={2}
+              hideGrid={true}
+              brushColor="#fff"
+              style={{ margin: "20px" }}
+              canvasHeight={400}
+              canvasWidth={300}
+              lazyRadius={0}
+            ></CanvasDraw>
+          </Col>
+        </Row>
+        <Row className="justify-content-md-center">
+          <Col>
+            <Button
+              style={{ margin: "5px" }}
+              variant="warning"
+              onClick={() => {
+                saveableCanvas.undo();
+              }}
+            >
+              Undo
+            </Button>
+            <Button
+              style={{ margin: "5px" }}
+              variant="danger"
+              onClick={() => {
+                saveableCanvas.clear();
+              }}
+            >
+              Clear
+            </Button>
+          </Col>
+        </Row>
+        <Row>
+          <Col className="justify-content-md-center">
+            <Button
+              style={{ width: "250px", margin: "25px" }}
+              onClick={() => {
+                console.log(saveableCanvas.getSaveData());
+                window.ReactNativeWebView.postMessage(
+                  saveableCanvas.getSaveData()
+                );
+              }}
+            >
+              Submit
+            </Button>
+          </Col>
+        </Row>
       </Container>
     )
   );
